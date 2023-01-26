7c9607c9e594042fc0b9ec2c101c927257532b68
well0lins <wellington4lins@outlook.com>
2023-01-26 14:35:14 -0300

    webpack commit

---
+++ public/assets/js/bundle.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+// 1
+// 2
+// 3
+
+console.log('Apenas um teste');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
